b6c3a139ee084f7b1d7df4348988a03e4d9699e6
Klas Kalaß <klas.r.kalass@googlemail.com>
2010-12-04 21:55:35 +0100

    use jquery ui to display information about an event, some styling changes

--- hqclient/build.sh
+++ hqclient/build.sh
@@ -7,7 +7,7 @@
 HQCLIENT_BASEDIR=${HQCLIENT_BASEDIR=`dirname $0`}
 PROJ_BASEDIR=${PROJ_BASEDIR=$HQCLIENT_BASEDIR/..}
 POLYMAPS_BASEDIR=${POLYMAPS_BASEDIR=$PROJ_BASEDIR/../polymaps/}
-
+JQUERY_UI_BASEDIR=${JQUERY_UI_BASEDIR=$PROJ_BASEDIR/../jquery-ui-1.8.6.custom}
 TARGET=${TARGET=$HQCLIENT_BASEDIR/target}
 
 if ! [ -d "$POLYMAPS_BASEDIR" ] ; then
@@ -23,8 +23,14 @@ rsync -a --delete $HQCLIENT_BASEDIR/src/html/ $TARGET/
 
 # 'compile' js
 rsync -a --delete $HQCLIENT_BASEDIR/src/js/ $TARGET/js/
+
+# include 3rdparty stuff
 [ -d $TARGET/js/lib/ ] || mkdir -p $TARGET/js/lib/
 cp $POLYMAPS_BASEDIR/polymaps.js $TARGET/js/lib/
+cp $JQUERY_UI_BASEDIR/js/jquery-ui-1.8.6.custom.min.js $TARGET/js/lib/
 
 # 'compile' css
 compass compile --sass-dir src/sass/ --images-dir src/img/ --javascripts-dir src/js/ --css-dir target/css/
+
+# 3rdparty css
+rsync -a --delete $JQUERY_UI_BASEDIR/css/ui-lightness/ $TARGET/css/ui-lightness/
