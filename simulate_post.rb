0018ccf40790d0f4d37b4ce856bbeb35523a3be8
Shoaib Burq <saburq@gmail.com>
2010-12-05 15:44:51 +0100

    Added the pusher service and changed simulater for local posts.

--- simulate_post.rb
+++ simulate_post.rb
@@ -12,14 +12,15 @@ markers= ['/markers/accident.png',
 '/markers/revolution.png',
 '/markers/strike.png']
 
-
+while true
 markers.each do |marker|
-  lon=(13.4113999+rand(100)/10000.0 - 0.00005)
-  lat=(52.5234051+rand(100)/10000.0 - 0.00005)
+  lon=(13.4131108954413+rand(100)/10000.0 - 0.00005)
+  lat=(52.50242228445892+rand(100)/10000.0 - 0.00005)
   
-  cmd = "curl -d \'{\"marker\":\"#{marker}\",\"description\":\"House belongestroyed, family homeless\",\"location\":[#{lon},#{lat}],\"tags\":[\"homeless\",\"bridge\",\"cut-off-supply-route\"]}\' -H \'Content-Type: application/json\' http://192.168.1.63:9292/api/v1/events"
+  cmd = "curl -d \'{\"marker\":\"#{marker}\",\"description\":\"House destroyed, family homeless\",\"location\":[#{lon},#{lat}],\"tags\":[\"homeless\",\"bridge\",\"cut-off-supply-route\"]}\' -H \'Content-Type: application/json\' http://192.168.1.63:9292/api/v1/events"
 
   puts cmd
   system(cmd)
   # sleep()
+end
 end
