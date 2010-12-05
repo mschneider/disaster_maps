ed38dee880398d0694006e02fca15e7b6572a51c
Shoaib Burq <saburq@gmail.com>
2010-12-05 13:49:22 +0100

    Added smaller rand.

--- simulate_post.rb
+++ simulate_post.rb
@@ -14,8 +14,8 @@ markers= ['/markers/accident.png',
 
 
 markers.each do |marker|
-  lon=(13.4113999+rand(100)/100.0 - 0.5)
-  lat=(52.5234051+rand(100)/100.0 - 0.5)
+  lon=(13.4113999+rand(100)/10000.0 - 0.00005)
+  lat=(52.5234051+rand(100)/10000.0 - 0.00005)
   
   cmd = "curl -d \'{\"marker\":\"#{marker}\",\"description\":\"House belongestroyed, family homeless\",\"location\":[#{lon},#{lat}],\"tags\":[\"homeless\",\"bridge\",\"cut-off-supply-route\"]}\' -H \'Content-Type: application/json\' http://192.168.1.63:9292/api/v1/events"
 
